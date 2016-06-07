21106ca86b632094671aa0fe550e9d0b19291319
nakitadog <aaron@softwarepromotions.com>
2016-06-07 18:24:06 -0500

    Update adwords-mcc-level-final-url-monitoring.js

--- adwords-mcc-level-final-url-monitoring.js
+++ adwords-mcc-level-final-url-monitoring.js
@@ -163,13 +163,18 @@ function getAllSitelinkURLs(account) {
   return SiteLinkURLs.sort();
 }  
 
-
-function getUrlStatus(url) {
-  var response = UrlFetchApp.fetch(url, { muteHttpExceptions: true});
-  if (response.getResponseCode()!== 200){
-    Logger.log("URL Error: %s \n %s",response.getResponseCode(), response);  
-  }  
-  return response.getResponseCode();
+function getUrlStatus(url) {  
+  try{
+    var response = UrlFetchApp.fetch(url, { muteHttpExceptions: true, validateHttpsCertificates: true});
+    
+    if (response.getResponseCode()!== 200){
+      Logger.log("URL Error: %s \n %s",response.getResponseCode(), response);
+    }
+    return response.getResponseCode();
+  } catch(e){
+    Logger.log("URL Error: %s \n %s",url, e);
+    return e
+  }
 }
 
 function sendEmail(emailToAddresses, myBody) {
